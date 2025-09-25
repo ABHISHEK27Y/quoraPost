b8350493c6f54d45466ff98bcf93bce7aacff76c
ABHISHEK27Y <abhishekyadav270705@gmail.com>
2025-09-25 08:57:35 +0530

    port added

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-const PORT = process.env.PORT || 5000;
-
+const port=8080;
 app.use(express.static(path.join(__dirname,"public")));
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.static("public"));
