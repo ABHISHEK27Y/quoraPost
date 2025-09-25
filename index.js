836e66a98c46b87bf86a1bba326d9ded651ca40a
ABHISHEK27Y <abhishekyadav270705@gmail.com>
2025-09-25 08:44:16 +0530

    port added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-const port=8080;
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(path.join(__dirname,"public")));
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.static("public"));
@@ -79,6 +80,10 @@ app.delete("/posts/:id",(req,res)=>{
      res.redirect("/posts")
 })
 
+// Start server
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
 
 
 
